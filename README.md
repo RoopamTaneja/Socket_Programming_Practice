7dea202f431d0c547c505fd7e712fb9088d671f2
RoopamTaneja <roopamtaneja999@gmail.com>
2024-10-04 00:25:59 +0530

    epoll started

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Socket Programming Practice
 
 My codes while learning socket programming.
+
+For theory, refer socket_programming.md and concurrent_io.md in Networks\md_notes of Tech Stuff.
+
+**Cheers !!**
---
+++ epoll_level_triggered.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <string.h>
+
+#define MAX_EVENTS 5
+#define READ_SIZE 10
+
+int main()
+{
+    int epoll_fd = epoll_create1(0); // create an epoll instance
+    if (epoll_fd == -1)
+    {
+        fprintf(stderr, "Failed to create epoll file descriptor\n");
+        return 1;
+    }
+
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = 0;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &event))
+    { // adds option specified in event to the epoll instance
+        fprintf(stderr, "Failed to add file descriptor\n");
+        close(epoll_fd);
+        return 1;
+    }
+
+    size_t bytes_read;
+    char read_buffer[READ_SIZE + 1]; // for storing data read
+    int event_cnt;
+    struct epoll_event events[MAX_EVENTS];
+    int running = 1;
+    while (running)
+    {
+        printf("\nPolling for input...\n");
+        event_cnt = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000); // wait on epoll for 30s at a time
+        printf("%d ready events\n", event_cnt);
+        for (int i = 0; i < event_cnt; i++)
+        {
+            printf(" -- Reading fd '%d' -- \n", events[i].data.fd);
+            bytes_read = read(events[i].data.fd, read_buffer, READ_SIZE);
+            read_buffer[bytes_read] = '\0';
+            printf("Read : %s\n", read_buffer);
+            if (!strncmp(read_buffer, "stop\n", 5))
+                running = 0;
+        }
+    }
+    if (close(epoll_fd))
+    {
+        fprintf(stderr, "Failed to close epoll file descriptor\n");
+        return 1;
+    }
+    return 0;
+}
